a5594c9fef59b12f50ac43a1bd86847d03714d4d
Kaquaoify <52884984+Kaquaoify@users.noreply.github.com>
2021-02-18 16:49:28 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,154 @@
+var config =
+{    
+    /*
+        Do we want to show the image?
+        Note that imageSize still affects the size of the image and where the progressbars are located.
+    */
+    enableImage: true,
+ 
+    /*
+        Relative path the the logo we want to display.
+    */
+    image: "img/logo.png",
+
+    /*
+        Cursor image
+    */
+    cursorImage: "img/cursor.png",
+ 
+    /*
+        How big should the logo be?
+        The values are: [width, height].
+        Recommended to use square images less than 1024px.
+    */
+    imageSize: [512, 512],
+ 
+    /*
+        Define the progressbar type
+            0 = Single progressbar
+            1 = Multiple progressbars
+            2 = Collapsed progressbars
+     */
+    progressBarType: 1,
+ 
+    /*
+        Here you can disable some of progressbars.
+        Only applys if `singleProgressbar` is false.
+    */
+    progressBars:
+    {
+        "INIT_CORE": {
+            enabled: false, //NOTE: Disabled because INIT_CORE seems to not get called properly. (race condition).
+        },
+ 
+        "INIT_BEFORE_MAP_LOADED": {
+            enabled: true,
+        },
+ 
+        "MAP": {
+            enabled: true,
+        },
+ 
+        "INIT_AFTER_MAP_LOADED": {
+            enabled: true,
+        },
+ 
+        "INIT_SESSION": {
+            enabled: true,
+        }
+    },
+ 
+    /*
+        Loading messages will be randomly picked from the array.
+        The message is located on the left side above the progressbar.
+        The text will slowly fade in and out, each time with another message.
+        You can use UTF-8 emoticons inside loading messages!
+    */
+    loadingMessages:
+    [
+        "Démarrage d'Internet Explorer",
+        "Mise à jour des touches de clavier",
+        "Inspection des caves",
+        "Traitement des cultures de coca",
+        "Indexation du rapport 05_complot-président_05406XD3.txt",
+        "Nettoyage des vitres de la voiture à Polo",
+        "Envoi de la cargaison d'armes vers Los Santos",
+        "Rappel de l'agent Trevor au secteur B-04-D6",
+        "Mise à jour des données non sécurisées",
+        "Ce message est faux, écoutez Michael, il dit la vérité",
+        "Michael n'existe pas, c'est une imagination de votre cerveau",
+    ],
+ 
+    /*
+        Rotate the loading message every 5000 milliseconds (default value).
+    */
+    loadingMessageSpeed: 5 * 1000,
+ 
+    /*
+        Array of music id's to play in the loadscreen.
+        Enter your youtube video id's here. In order to obtain the video ID
+        Take whats after the watch?v= on a youtube link.
+        https://www.youtube.com/watch?v=<videoid>
+        Do not include the playlist id or anything, it should be a 11 digit code.
+       
+        Do not use videos that:
+            - Do not allow embedding.
+            - Copyrighted music (youtube actively blocks this).
+    */
+    music:
+    [
+        "OZcoDQybozk", "ap1wPAMOjXs", "H0h6GEFek5w",
+        "Nds2d2YsFsw", "Yb4U3BB-3iQ", "XVnvgycRU9k",
+        "OZcoDQybozk", "0z3WUfDfryA", "hDCeAWNWhfU",
+        "vWdjWYhEONA", "SoMeB4QBVug", "8sV6AT6jVuI",
+    ],
+ 
+ 
+    /*
+        Set to false if you do not want any music.
+    */
+    enableMusic: true,
+ 
+    /*
+        Default volume for the player. Please keep this under 50%, to not blowout someones eardrums x)
+     */
+    musicVolume: 10,
+ 
+    /*
+        Should the background change images?
+        True: it will not change backgrounds.
+        False: it will change backgrounds.
+    */
+    staticBackground: false,
+   
+    /*
+        Array of images you'd like to display as the background.
+        Provide a path to a local image, using images via url is not recommended.
+    */
+    background:
+    [
+        "img/bg1.jpg",
+        "img/bg2.jpg",
+        "img/bg3.jpg",
+    ],
+ 
+    /*
+        Time in milliseconds on how fast the background
+        should swap images.
+     */
+    backgroundSpeed: 10 * 1000,
+
+    /*
+        Which style of animation should the background transition be?
+        zoom = background zooms in and out.
+        slide = transtion backgrounds from sliding right and back again.
+        fade = fade the background out and back in.
+    */
+    backgroundStyle: "zoom",
+
+    /*
+        An on-screen logger, for indicating breaking bugs! :(
+        Should we enable the on-screen logger? Handy for debugging or indicating errornous behaviour!
+    */
+    enableLog: true,
+}
